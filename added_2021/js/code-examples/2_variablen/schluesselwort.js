c9651921f39583b738e2b1536bc502e43b48eb2a
Christoph Haefner <christoph.haefner@mailbox.org>
2022-02-19 18:14:53 +0100

    Add js day1 2021

---
+++ added_2021/js/code-examples/2_variablen/schluesselwort.js
@@ -0,0 +1,8 @@
+let a = "Hello World"; // Variable
+const b = 'Hello World'; // Konstante
+
+// TypeError: Assignment to constant variable
+b = "Not again";
+
+// Altes JavaScript
+var c = "Hello World"; // Variable
