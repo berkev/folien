8ad1f48922eff08f560b47c114747f14ba54e919
Christoph Haefner <christoph.haefner@mailbox.org>
2024-02-13 21:01:31 +0100

    Add js for TINF23B2 day 1

---
+++ TINF23B2/js/code-examples/02_variablen/datentypen.js
@@ -0,0 +1,11 @@
+a = "Hello World"; // Zeichenkette (String)
+a = 'Hello World'; // Zeichenkette (String)
+
+a = 4711; // Zahl (Number)
+a = -34.456E-34; // Zahl (Number)
+
+a = true; // Wahrheitswert (Boolean)
+a = false; // Wahrheitswert (Boolean)
+
+a = null; // undefiniert
+a = undefined; // undefiniert
